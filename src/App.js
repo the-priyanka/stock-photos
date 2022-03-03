0b55c6bd6a45993d339c89035c1eb918416dbfc6
Priyanka <original.manaria@gmail.com>
2022-03-03 11:59:22 +0530

    stock photo -- initial Fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
+import { useEffect, useState } from "react";
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}?client_id=ZAcztqd9EgDXiCpFNVZTuy90GVfZvAYXSSFKYUyO9Jo`;
+    try {
+      let response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <div className="App">
       <h1>Stock photos starter!</h1>
